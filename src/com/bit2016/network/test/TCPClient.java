1a0997c4742216ac7d0237907fa599d2ee85b91a
BIT-USER <BIT-USER@BIT>
2016-09-30 16:30:22 +0900

    30일

--- src/com/bit2016/network/test/TCPClient.java
+++ src/com/bit2016/network/test/TCPClient.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class TCPClient {
 	private static final String SERVER_IP = "192.168.1.6";
@@ -16,6 +17,23 @@ public class TCPClient {
 			// 1. socket 생성
 			socket = new Socket();
 
+			// 1-1 socket buffer size 확인
+			int receiveBufferSize = socket.getReceiveBufferSize();
+			int SendBufferSize = socket.getSendBufferSize();
+			
+			// 1-2 socket buffer size 변경
+			socket.setReceiveBufferSize(10 * 1024);
+			socket.setSendBufferSize(10 * 1024);
+			
+			receiveBufferSize = socket.getReceiveBufferSize();
+			SendBufferSize = socket.getSendBufferSize();
+			System.out.println(receiveBufferSize +":"+SendBufferSize);
+			// 1-3 SO_NODELAY (Nagle Alogorithm Off)
+			socket.setTcpNoDelay(true);
+			
+			// 1-4 SO_TIMEOUT //5번 읽기에서 timeout을 걸고싶은것!
+			socket.setSoTimeout(3000);
+			
 			// 2. 서버연결
 			socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
 			System.out.println("[client] connected");
@@ -39,9 +57,11 @@ public class TCPClient {
 			data = new String(buffer, 0, readByteCount,"UTF-8");
 			System.out.print("[client] received:" + data);
 			
-		} catch (IOException e) {
+		}  catch(SocketTimeoutException ex){
+			System.out.println("[client time out]");
+		}catch (IOException e) {
 			e.printStackTrace();
-		} finally {
+		}finally {
 			try {
 				if (socket != null && socket.isClosed() == false)
 					socket.close();
